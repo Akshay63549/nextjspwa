44698e6ad989ad2b8d165042af63b4729fa96929
Your Name <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>
2024-02-13 16:39:51 +0530

    some changes

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+import runtimeCaching from "next-pwa/cache.js";
+
+import WithPWA from "next-pwa";
+const withPWA = WithPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable:false
+});
+
+export default withPWA({
+  // other configs
+  reactStrictMode: false,
+});
